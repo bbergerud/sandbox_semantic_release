6f4a992c47b520f97934aae34c66afb363a39fd8
Brandon Bergerud <brandon.bergerud@gmail.com>
2024-06-06 08:16:59 -0400

    init commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+from setuptools import setup
+
+__version__ = "0.0.0"
+
+setup(
+    name="my-package",
+    version=__version__,
+)
+
+
+try:
+    from semantic_release import setup_hook
+
+    setup_hook(sys.argv)
+except ImportError:
+    pass
